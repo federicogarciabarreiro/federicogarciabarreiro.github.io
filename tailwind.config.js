b0ad131a1c85dc28cc193cda0475b69ae8b3d025
fguniversae <166795294+fguniversae@users.noreply.github.com>
2024-05-07 10:10:35 +0200

    init nextui pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,16 @@
-// tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+import {nextui} from '@nextui-org/react'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // ...
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [
-    nextui({
-      themes: {
-        light: {
-          colors: {
-            primary: "#FFD34E",
-            secondary: "#EE457E",
-            background:"#F4E8D1"
-          }
-        },
-        dark: {
-          colors: {
-            primary: "#FFD34E",
-            secondary: "#EE457E",
-            background: "#E1CA9E"
-          }
-        },
-      }
-    })
-  ],
-};
+  plugins: [nextui()],
+}
