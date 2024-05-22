9c1098f5cadf28f95d9a29cfd52a064bae373e6a
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-05-22 23:15:56 +0200

    update logos
    carousel need fix
    divide card from other comps

--- src/components/CustomCarousel.js
+++ src/components/CustomCarousel.js
@@ -7,27 +7,31 @@ import "../styles/CustomCarousel.css";
 
 function CustomCarousel({ images }) {
     return (
-        <div className='margin-top-bot'>
-            <CustomSection children={<Carousel>
-                {images.map((image, index) => (
-                    <Carousel.Item key={index}>
-                        <div className="carousel-image-container">
-                            <img
-                                className="d-block w-100"
-                                src={image.src}
-                                alt={image.title}
-                            />
-                        </div>
-                        <div className="carousel-caption-container">
-                            <Carousel.Caption>
-                                <h5 className=''>{image.title}</h5>
-                                <p className=''>{image.description}</p>
-                            </Carousel.Caption>
-                        </div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>} />
-        </div>
+
+        <div className="card">
+            
+
+                        <CustomSection children={<Carousel>
+                            {images.map((image, index) => (
+                                <Carousel.Item key={index}>
+                                    <div className="carousel-image-container">
+                                        <img
+                                            className="d-block w-100"
+                                            src={image.src}
+                                            alt={image.title}
+                                        />
+                                    </div>
+                                    <div className="carousel-caption-container">
+                                        <Carousel.Caption>
+                                            <h5 className=''>{image.title}</h5>
+                                            <p className=''>{image.description}</p>
+                                        </Carousel.Caption>
+                                    </div>
+                                </Carousel.Item>
+                            ))}
+                        </Carousel>} />
+                    </div>
+       
     );
 }
 
