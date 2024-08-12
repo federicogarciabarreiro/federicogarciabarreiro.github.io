00cb473bdcf3b06655252bff11e6406538880eac
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-08-12 21:15:01 +0200

    clear

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
-export default defineConfig({
-  site: 'https://federicogarciabarreiro.github.io',
-  base: 'porfolio',
-  integrations: [react(), tailwind()],
-})
