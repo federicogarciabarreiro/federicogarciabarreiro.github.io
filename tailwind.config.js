f36040bdb55cde99e873e8cd0ace77b3aba030b9
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-08-12 19:42:52 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { nextui } = require("@nextui-org/react");
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx,astro}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js",
   ],
   theme: {
     extend: {},
