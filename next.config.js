02e66b92a529906553df37c50a33b9069fa282cd
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-08-12 21:53:19 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = "production"
+
 const nextConfig = {
+  basePath: isProd? "/porfolio" : "",
   output: 'export',
   reactStrictMode: true,
   distDir: 'dist'
