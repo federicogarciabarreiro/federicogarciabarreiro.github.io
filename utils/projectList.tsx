acc958336c95d20d190d4c321e94c7d3a34df376
Federico Garcia Barreiro <federicogarciabarreiro@hotmail.com>
2024-09-10 11:04:05 +0200

    Update

--- utils/projectList.tsx
+++ utils/projectList.tsx
@@ -1,57 +1,61 @@
-import { ExpressSVG, MongoDBSVG, MongooseSVG, SwaggerSVG, TypescriptSVG } from "@/components/ui/icons";
+import { UnitySVG } from "@/components/ui/icons";
 import { ProjectType } from "../types";
 
 export const ProjectList: ProjectType[] = [
   {
-    icon:  <TypescriptSVG
-    color="#3178C6"
+    icon:  <UnitySVG
+    color="#47A248"
     size={16}
   />,
-    title: "Shop API",
-    subtitle: "E-commerse back-end",
+    title: "Sediento",
+    subtitle: "Juego del tipo puzzle, diseñado para la Indie Spain Jam 2023.",
     technologies: [
       {
-        name: "Express",
-        icon: (
-          <ExpressSVG
-            color="#000000"
-            size={16}
-          />
-        ),
+        name: "Godot Engine",
+        icon: (<></>),
       },
       {
-        name: "MongoDB",
-        icon: (
-          <MongoDBSVG
-            color="#47A248"
-            size={16}
-          />
-        ),
+        name: "Godot Script",
+        icon: (<></>),
       },
       {
-        name: "Mongoose",
-        icon: (
-          <MongooseSVG
-            color="#880000"
-            size={16}
-          />
-        ),
-      },
-      {
-        name: "Swagger",
-        icon: (
-          <SwaggerSVG
-            color="#85EA2D"
-            size={16}
-          />
-        ),
-      },
+        name: "Game Jam",
+        icon: (<></>),
+      }
     ],
     description:
-      "Backed e-commerce project that allows to manage users, categories, coupons and products with the endpoints ready to play with. Includes swagger docs.",
-    imgUrl: "/videos/shop-api.webp",
-    videoUrl: "",
-    githubUrl: "https://github.com/leomirandaa/shop-API.git",
-    websiteUrl: "NONE",
+      "Despiertas en una misteriosa habitación, junto a tu ser querido, pero algo está terriblemente mal. Debes explorar la habitación, resolver enigmas y abrir candados para escapar antes de que caiga la noche. Cada segundo cuenta, y el tiempo se agota rápidamente. ¿Podrás descubrir la verdad oculta detrás de esta pesadilla antes de que sea demasiado tarde?",
+    imgUrl: "/images/sediento.webp",
+    videoUrl: "/videos/sediento.mp4",
+    githubUrl: "https://github.com/federicogarciabarreiro/",
+    websiteUrl: "https://fedeegb.itch.io/sediento",
+  },
+  {
+    icon:  <UnitySVG
+    color="#47A248"
+    size={16}
+  />,
+    title: "Creamy Land",
+    subtitle: "Juego del tipo runner, diseñado de manera integral durante la carrera.",
+    technologies: [
+      {
+        name: "Unity Engine",
+        icon: (<></>),
+      },
+      {
+        name: "Lenguaje C#",
+        icon: (<></>),
+      },
+      {
+        name: "Carrera",
+        icon: (<></>),
+      }
+    ],
+    description:
+      "¡Encarna a los valientes hermanos Leafcream y Firecream en Creamy Land! Corre a través de un mundo helado lleno de desafíos para rescatar a la princesa Lovecream. Supera obstáculos, recoge premios y restaura la paz en este dulce reino. ¡Tu aventura comienza ahora!",
+    imgUrl: "/images/sediento.webp",
+    videoUrl: "/videos/sediento.mp4",
+    githubUrl: "https://github.com/federicogarciabarreiro/",
+    websiteUrl: "",
   }
 ];
