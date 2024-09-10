150fcb12e655cc4923603745b86234baa3dc4666
Federico Garcia Barreiro <federicogarciabarreiro@hotmail.com>
2024-09-10 12:11:28 +0200

    Update

--- utils/projectList.tsx
+++ utils/projectList.tsx
@@ -7,6 +7,50 @@ export const ProjectList: ProjectType[] = [
     color="#47A248"
     size={16}
   />,
+    title: "Porfolio",
+    subtitle: "Porfolio actualizado al año 2024.",
+    technologies: [
+      {
+        name: "Visual Studio Code",
+        icon: (<></>),
+      },
+      {
+        name: "React",
+        icon: (<></>),
+      },
+      {
+        name: "Astro",
+        icon: (<></>),
+      },
+      {
+        name: "Typescript",
+        icon: (<></>),
+      },
+      {
+        name: "Tailwind",
+        icon: (<></>),
+      },
+      {
+        name: "GitHub",
+        icon: (<></>),
+      },
+      {
+        name: "GitHub Pages",
+        icon: (<></>),
+      }
+    ],
+    description:
+      "Porfolio actualizado al año 2024.",
+    imgUrl: "/images/porfolio.webp",
+    videoUrl: "/videos/sediento.mp4",
+    githubUrl: "https://github.com/federicogarciabarreiro/federicogarciabarreiro.github.io",
+    websiteUrl: "https://federicogarciabarreiro.github.io",
+  },
+  {
+    icon:  <UnitySVG
+    color="#47A248"
+    size={16}
+  />,
     title: "Sediento",
     subtitle: "Juego del tipo puzzle, diseñado para la Indie Spain Jam 2023.",
     technologies: [
@@ -20,6 +64,22 @@ export const ProjectList: ProjectType[] = [
       },
       {
         name: "Game Jam",
+        icon: (<></>),
+      },
+      {
+        name: "Diseño de personajes",
+        icon: (<></>),
+      },
+      {
+        name: "Animacion",
+        icon: (<></>),
+      },
+      {
+        name: "Aseprite",
+        icon: (<></>),
+      },
+      {
+        name: "GitHub",
         icon: (<></>),
       }
     ],
@@ -35,8 +95,8 @@ export const ProjectList: ProjectType[] = [
     color="#47A248"
     size={16}
   />,
-    title: "Creamy Land",
-    subtitle: "Juego del tipo runner, diseñado de manera integral durante la carrera.",
+    title: "Switch",
+    subtitle: "Juego del tipo runner 3D, diseñado en grupo durante mi carrera en el año 2020.",
     technologies: [
       {
         name: "Unity Engine",
@@ -47,7 +107,55 @@ export const ProjectList: ProjectType[] = [
         icon: (<></>),
       },
       {
-        name: "Carrera",
+        name: "MS Visual Studio",
+        icon: (<></>),
+      },
+      {
+        name: "GitHub",
+        icon: (<></>),
+      }
+    ],
+    description:
+      "¡Desafía tus reflejos en Switch! Controla una nave que se desplaza a toda velocidad a través de un tubo infinito, cambiando de dirección con un solo clic. El entorno responde al ritmo de la música, creando una experiencia visual y auditiva única. ¿Cuánto tiempo podrás mantener el ritmo y esquivar los obstáculos?",
+    imgUrl: "/images/sediento.webp",
+    videoUrl: "/videos/sediento.mp4",
+    githubUrl: "https://github.com/federicogarciabarreiro/",
+    websiteUrl: "",
+  },
+  {
+    icon:  <UnitySVG
+    color="#47A248"
+    size={16}
+  />,
+    title: "Creamy Land",
+    subtitle: "Juego del tipo runner 2D, diseñado de manera integral durante la carrera.",
+    technologies: [
+      {
+        name: "Unity Engine",
+        icon: (<></>),
+      },
+      {
+        name: "Lenguaje C#",
+        icon: (<></>),
+      },
+      {
+        name: "Diseño de personajes",
+        icon: (<></>),
+      },
+      {
+        name: "Animacion",
+        icon: (<></>),
+      },
+      {
+        name: "Adobe Illustrator",
+        icon: (<></>),
+      },
+      {
+        name: "MS Visual Studio",
+        icon: (<></>),
+      },
+      {
+        name: "GitHub",
         icon: (<></>),
       }
     ],
