06516e10e3207a1bbc82ac3287232f03d19d0fcf
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-10-26 04:22:14 +0200

    Confetti Update

--- components/downloadCVButton.tsx
+++ components/downloadCVButton.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button } from "@nextui-org/button";
+import confetti from 'canvas-confetti';
 
 const DownloadCVButton = () => {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const handleConfetti = () => {
+    if (buttonRef.current) {
+      const rect = buttonRef.current.getBoundingClientRect();
+      const x = rect.left + rect.width / 2;
+      const y = rect.top + rect.height / 2;
+
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: x / window.innerWidth, y: y / window.innerHeight }
+      });
+    }
+  };
+
   return (
     <a href="/assets/cv.pdf" download>
-      <Button>¡Descargar CV!</Button>
+      <Button
+        ref={buttonRef}
+        disableRipple
+        className="relative overflow-visible rounded-full hover:-translate-y-1 px-12 shadow-xl bg-background/30 after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0"
+        size="lg"
+        onPress={handleConfetti}
+      >
+        ¡Descargar CV!
+      </Button>
     </a>
   );
 };
