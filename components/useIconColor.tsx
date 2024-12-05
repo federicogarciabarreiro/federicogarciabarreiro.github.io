f3957537e62df028f8908458487402b7c982b407
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-12-05 14:06:44 +0100

    Update
    Color fixed allways white

--- components/useIconColor.tsx
+++ components/useIconColor.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
+{/*Determine tener un solo color, en este caso white, pero deje la logica preparada para interactuar con ambos por si fuera necesario...*/}
+
 export const useIconColor = () => {
   const { resolvedTheme } = useTheme();
-  const [iconColor, setIconColor] = useState((resolvedTheme === "light" ? "white" : "black"));
+  const [iconColor, setIconColor] = useState((resolvedTheme === "light" ? "white" : "white"));
 
   useEffect(() => {
-    setIconColor(resolvedTheme === "light" ? "white" : "black");
+    setIconColor(resolvedTheme === "light" ? "white" : "white");
   }, [resolvedTheme]);
 
   return iconColor;
